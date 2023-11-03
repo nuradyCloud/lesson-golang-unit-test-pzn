75faa03d5856f0a93bd7a193928bc0ec5448d826
nuradyCloud <nurady.pamungkas@gmail.com>
2023-11-03 15:56:15 +0700

    update dir path for other package based dir project

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -1,6 +1,6 @@
 package repository
 
-import "github.com/lesson-golang-unit-test-pzn/entity"
+import "github.com/nuradyCloud/lesson-golang-unit-test-pzn/entity"
 
 type CategoryRepository interface {
 	FindById(id string) *entity.Category
